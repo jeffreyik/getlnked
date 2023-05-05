7cb220572f145e9971f7fb1053b8b0af6fe3c4b9
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-05 07:49:12 +0100

    Fixed image src

--- src/components/TemplateSection.tsx
+++ src/components/TemplateSection.tsx
@@ -9,7 +9,7 @@ const TemplateSection = () => {
                 <p className="text-[16px] text-neutralGray">Our platform supports several portfolio templates that fits your style or need.</p>
                 <button className="neo w-[226px] h-[55px] rounded-[10px]">Get started</button>
             </div>
-            <Image src="/assets/group 1.svg" width={399} height={436.42} alt="" />
+            <Image src="./assets/group 1.svg" width={399} height={436.42} alt="" />
         </div>
     </section>
   )
