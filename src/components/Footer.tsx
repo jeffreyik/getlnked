77edb2800b7db2c1fb0aa7c7c86c85f9ceef17cd
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-05 07:57:17 +0100

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='container m-auto text-center py-24'>
-        <p className='text-[18px] font-normal text-gray'>🚀 getlnked copyright@2023</p>
+      <p className='text-[18px] font-normal text-gray'>🚀 getlnked copyright@2023</p>
     </footer>
   )
 }
