aca59689108746ab6d2bd74858d40843ab08cfda
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-07 12:49:56 +0100

    Fixed input width

--- src/components/TemplateSection.tsx
+++ src/components/TemplateSection.tsx
@@ -4,7 +4,7 @@ const TemplateSection = () => {
   return (
     <section className="bg-lightGreen py-24">
         <div className="gap-24 container mx-auto flex flex-col items-center justify-between lg:space-x-32 lg:flex-row">
-            <div className="md:space-y-7">
+            <div className="space-y-7">
                 <h1 className="text-[32px] md:text-[48px] font-medium">Choose a template that fits your style</h1>
                 <p className="text-[16px] md:text-[18px] text-neutralGray">Our platform supports several portfolio templates that fits your style or need.</p>
                 <button className="neo w-[226px] h-[55px] rounded-[10px]">Get started</button>
--- src/components/forms/ClaimUsername.tsx
+++ src/components/forms/ClaimUsername.tsx
@@ -7,7 +7,7 @@ const ClaimUsername = ({ text }: Label) => {
     <form className="flex items-center sm:gap-4 w-full lg:w-[500px]">
         <div className="px-[15px] text-[15px] flex items-center border-black border-[1px] rounded-[10px] w-[70%] h-[55px]">
             <p className="font-medium">getlnked/</p>
-            <input className="text-[15px] outline-none" type="text" placeholder="Jeffrey" />
+            <input className="text-[15px] outline-none w-full" type="text" placeholder="Jeffrey" />
         </div>
         <button className="neo rounded-[10px] w-[30%] h-[55px]">{ text }</button>
     </form>
