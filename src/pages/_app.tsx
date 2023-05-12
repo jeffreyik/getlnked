e3e3a509a23875b749a1f353f6a4ee54d724329e
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-12 13:18:21 +0100

    added favicon and google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { NextPage } from 'next'
 import { ReactElement, ReactNode } from 'react'
+import Script from 'next/script'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
 
@@ -21,12 +22,18 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <main className={poppins.className}>
-      <Head>
-        <title>getlnked | Create a Stunning Portfolio Site with Ease 🚀</title>
-        <meta name="description" content="Create a professional portfolio or personal website with just a few clicks that showcases your work and tells your story in a way that's uniquely you." />
-      </Head>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-  
+      <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
        { getLayout(<Component {...pageProps} />) }
     </main>
   )
