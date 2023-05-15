5ee825b4085f52ae558bf0765978552a3b6fc2ca
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-15 18:19:38 +0100

    added ux copy when user joins waitlist

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ interface modalInterface {
 const Modal = ({ text, toggleModal }: modalInterface) => {
     return ( 
         <>
-            <div style={{display: toggleModal ? 'flex': 'none'}} className="w-full h-screen bg-neutralGray fixed z-10 flex items-center justify-center">
+            <div style={{display: toggleModal ? 'flex': 'none'}} className="w-full h-screen bg-neutralGray fixed flex items-center justify-center">
                 <div className="bg-white w-[280px] h-[180px] rounded-2xl p-10">
                     { text }
                 </div>
--- src/pages/waitlist.tsx
+++ src/pages/waitlist.tsx
@@ -107,9 +107,8 @@ const Waitlist = () => {
 
     return ( 
         <div className="flex flex-col items-center h-screen mx-auto max-w-[70em] w-[90%]">
-        {/* <Modal text="Congratulations on joining the waitlist" toggleModal={hasJoinedWaitlist} /> */}
           {hasJoinedWaitlist && <Confetti className="relative z-20" width={width} height={height} recycle={false} />}
-            <Image className="absolute top-0 w-full h-[24px] object-cover" src="/cover2.png" width={1440} height={54} alt="banner" />
+            <Image className="absolute top-0 w-full h-[24px] object-cover -z-10" src="/cover2.png" width={1440} height={54} alt="banner" />
             <div className="max-w-[415px] mt-24 flex flex-col justify-center items-center">
                 <Image className="mb-[27px]" src="/waitlistlogo.svg" width={106} height={107.57} alt="getlnked logo" />
                 <h1 className="text-[24px] mb-[27px] text-neutralGray text-center font-medium lg:text-[36px]">Join the waitlist <span className="text-black">to get early access</span></h1>
@@ -117,7 +116,7 @@ const Waitlist = () => {
                     <InputField type="text" placeholder="Name" setValue={setName} value={name} />
                     <InputField type="email" placeholder="Email address" setValue={setEmail} value={email} />
                     {error && <div className="text-red-500 text-[14px]">Contact already exists</div>}
-                    <button className="py-[15px] mt-[55px] neo w-full rounded-[10px] h-[49px]">{loading ? 'Loading...' : 'Join waitlist'}</button>
+                    <button className={`py-[15px] mt-[55px] neo w-full rounded-[10px] h-[49px] ${hasJoinedWaitlist && 'opacity-50'}`} disabled={hasJoinedWaitlist || loading}>{loading ? 'Loading...' : hasJoinedWaitlist ? 'Thank you for joining the waitlist 🚀': 'Join waitlist'}</button>
                 </form>
                 <Link href="https://chat.whatsapp.com/C5t6UsN7BYuHTCgnWlCAya" target="_blank" className="text-center text-[14px] mt-7 hover:underline">Join our whatsapp community</Link>
             </div>
