da391004b487a3125ba1ba97afb212804778393d
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-17 12:01:14 +0100

    added supabase to project

---
+++ src/supabase.ts
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl:string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+
+export const supabase = createClient(supabaseUrl ?? '', supabaseKey ?? '')
