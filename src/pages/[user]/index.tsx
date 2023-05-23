64e41f9224f29bdd35e28945189e9e84335fe9c3
jeffrey <ikechukwujeffrey19@gmail.com>
2023-05-23 14:45:21 +0100

    Removed case sensitivity to username path

--- src/pages/[user]/index.tsx
+++ src/pages/[user]/index.tsx
@@ -32,7 +32,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const { data: userData, error } = await supabase
         .from('users')
         .select('*')
-        .eq("username", user)
+        .eq("username", user.toLocaleLowerCase())
         .single()
 
         if (!userData || error) {
@@ -51,42 +51,4 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
 }
 
-// export const getStaticProps: GetStaticProps = async (context) => {
-//     const { user } = context.params as Params
-//     const { data, error } = await supabase
-//       .from('users')
-//       .select('*')
-//     const userData = data?.find(singleUser => singleUser.username === user)
-
-//     if (!userData) {
-//         return {
-//             props: {},
-//             notFound: () => <h1>Page not found</h1>
-//         }
-//     }
-
-//     return { 
-//         props: {
-//             userData
-//         }
-//     }
-// }
-
-// export const getStaticPaths = async () => {
-//     const { data, error } = await supabase
-//       .from('users')
-//       .select('*')
-
-//       const paths = data?.map(user => {
-//         return {
-//             params: { user : user.username }
-//         }
-//       })
-
-//       return {
-//         fallback: true,
-//         paths,
-//     }
-// }
-  
- export default Page;
+export default Page;
